51807c347423dc90ef3e0ca471b9da402b2ac8d0
HansikaPerera1117 <hansikaperera59@gmail.com>
2024-08-02 22:41:23 +0530

    add project to github

---
+++ src/pages/Pages/User Mangement/UserManagement.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container, Card, Row, Col, Label, Input, FormGroup } from "reactstrap";
+import { Table } from "antd";
+
+const UserManagement = () => {
+  return (
+    <div className="page-content">
+      <h4>User Management</h4>
+    </div>
+  );
+};
+
+export default UserManagement;
