113e86a999ea639022a96d73d9f1e4b3da708bba
HansikaPerera1117 <hansikaperera59@gmail.com>
2024-08-04 13:18:45 +0530

    add product service file

---
+++ src/service/productService.js
@@ -0,0 +1,51 @@
+import ApiService from "./apiService";
+
+export async function getAllProducts() {
+  const apiObject = {};
+  apiObject.method = "GET",
+  apiObject.authentication = true,
+  apiObject.isWithoutPrefix = false;
+  apiObject.endpoint = ``;
+  apiObject.body = null;
+  return await ApiService.callApi(apiObject);
+}
+
+export async function addNewProduct(data) {
+  const apiObject = {};
+  apiObject.method = "POST",
+  apiObject.authentication = true,
+  apiObject.isWithoutPrefix = false;
+  apiObject.endpoint = ``;
+  apiObject.body = data;
+  return await ApiService.callApi(apiObject);
+}
+
+export async function updateProduct(productId, data) {
+  const apiObject = {};
+  apiObject.method = "PUT",
+  apiObject.authentication = true,
+  apiObject.isWithoutPrefix = false;
+  apiObject.endpoint = ``;
+  apiObject.body = data;
+  return await ApiService.callApi(apiObject);
+}
+
+export async function activeInactiveDeleteProduct(productId, status) {
+  const apiObject = {};
+  apiObject.method = "PATCH",
+  apiObject.authentication = true,
+  apiObject.isWithoutPrefix = false;
+  apiObject.endpoint = ``;
+  apiObject.body = null;
+  return await ApiService.callApi(apiObject);
+}
+
+export async function productsFiltration(data, currentPage) {
+  const apiObject = {};
+  apiObject.method = "GET";
+  apiObject.authentication = true;
+  apiObject.isWithoutPrefix = false;
+  apiObject.endpoint = ``;
+  apiObject.body = null;
+  return await ApiService.callApi(apiObject);
+}
