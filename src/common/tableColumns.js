05f56945f8e15eccc13395552e123b8f82c661ac
HansikaPerera1117 <hansikaperera59@gmail.com>
2024-08-16 15:14:59 +0530

    add category table colums

--- src/common/tableColumns.js
+++ src/common/tableColumns.js
@@ -118,3 +118,88 @@ export const RoleTableColumns = [
     render: (text, record) => <div>{record.action}</div>,
   },
 ];
+
+export const CategoriesTableColumns = [
+  {
+    title: "Image",
+    dataIndex: "image",
+    key: "image",
+
+    render: (text, record) => (
+      <div
+        style={{
+          maxWidth: 150,
+          minWidth: 100,
+        }}
+      >
+        {record.image}
+      </div>
+    ),
+  },
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Description",
+    dataIndex: "description",
+    key: "description",
+    render: (text) => {
+      let displayText = "";
+
+      if (typeof text === "string") {
+        displayText = text.slice(0, 550);
+
+        if (text.length > 550) {
+          const lastSpaceIndex = displayText.lastIndexOf(" ");
+          displayText = displayText.slice(0, lastSpaceIndex) + "...";
+        }
+      } else {
+        displayText = "Invalid description";
+      }
+
+      return (
+        <div
+          style={{
+            maxWidth: 800,
+            minWidth: 200,
+          }}
+        >
+          {parse(displayText)}
+        </div>
+      );
+    },
+  },
+  {
+    title: "Categories Status",
+    key: "categories_status",
+    width: "10%",
+    dataIndex: "categories_status",
+    render: (categories_status) => (
+      <Tag
+        color={
+          categories_status === 1
+            ? "success"
+            : categories_status === 2
+            ? "error"
+            : "default"
+        }
+        key={categories_status}
+      >
+        {categories_status === 1
+          ? "ACTIVE"
+          : categories_status === 2
+          ? "INACTIVE"
+          : "none"}
+      </Tag>
+    ),
+  },
+
+  {
+    title: "Action",
+    key: "action",
+    width: "20%",
+    render: (text, record) => <div>{record.action}</div>,
+  },
+];
